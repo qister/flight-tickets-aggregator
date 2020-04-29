34633d27079834f5cc3220e6c4be06e049cc25a6
Alexander Lyakhnitskiy <qistersick@gmail.com>
2020-04-29 23:12:09 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,491 @@
+const formSearch = document.querySelector('.form-search'),
+    inputCitiesFrom = document.querySelector('.input__cities-from'),
+    dropdownCitiesFrom = document.querySelector('.dropdown__cities-from'),
+    inputCitiesTo = document.querySelector('.input__cities-to'),
+    dropdownCitiesTo = document.querySelector('.dropdown__cities-to'),
+    inputDateDepart = document.querySelector('.input__date-depart'),
+    cheapestTicket = document.getElementById('cheapest-ticket'),
+    otherCheapTickets = document.getElementById('other-cheap-tickets'),
+    cheapestTicketSky = document.getElementById('cheapest-ticket-sky');
+
+
+const citiesApi = 'database/cities.json',
+    citiesApiFull = 'http://api.travelpayouts.com/data/ru/cities.json'
+    myProxy = 'https://cors-anywhere.herokuapp.com/',
+    API_KEY = 'adaf6ec8a9a81280f60cff61273b90c6',
+    calendar = 'http://min-prices.aviasales.ru/calendar_preload',
+    MAX_COUNT = 10;
+
+let city = [];
+
+//skyscanner
+
+let startCityId = 1;
+let startCityName = [1];
+let endCityId = 2;
+let endCityName = [2];
+let minPrice;
+let skyFetchDate;
+let dateSkyFrom;
+
+
+//
+// const getSkyFlights = (callback) => {
+// 	const request = new XMLHttpRequest();
+// 	request.withCredentials = true;
+
+// 	request.addEventListener('readystatechange', () => {
+// 			if (request.readyState !== 4) return;
+
+// 			if (request.status === 200) {
+// 				callback(request.response)
+// 			} else {
+// 				console.log(request.status);
+// 			}
+
+// 		});
+
+// 	request.open("GET", "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/RU/RUB/en-US/MOSC-sky/JFK-sky/2020-09-01");
+// 	request.setRequestHeader("x-rapidapi-host", "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com");
+// 	request.setRequestHeader("x-rapidapi-key", "be4393eccamsh39d62f04ce5e4f8p1492acjsnfbbd8746a9b8");
+
+
+
+// 	request.send(data);
+// }
+
+//Aviasales
+
+const getData = (url, callback, reject = console.error) => {
+    const request = new XMLHttpRequest();
+
+    request.open('GET', url);
+
+    request.addEventListener('readystatechange', () => {
+        if (request.readyState !== 4) return;
+
+        if (request.status === 200) {
+            callback(request.response)
+        } else {
+            reject(request.status);
+        }
+
+    });
+
+    request.send();
+
+};
+
+const showCity = (input, list) => {
+    list.textContent = ''; 
+
+    if (input.value !== '') {      
+
+        const filterCity = city.filter((item) => {
+            
+                const fixItem = item.name.toLowerCase();
+                return fixItem.startsWith(input.value.toLowerCase());
+
+            });
+
+            filterCity.forEach((item) => {
+                const li = document.createElement('li');
+                li.classList.add('dropdown__city');
+                li.textContent = item.name;
+                list.append(li);
+            });
+    }
+};
+
+const selectCity = (event, input, list) => {
+
+    const target = event.target;
+
+    if (target.tagName.toLowerCase() === 'li') {
+        input.value = target.textContent;
+        list.textContent = '';
+    }
+};
+
+const getNameCity = (code) => {
+    const objCity = city.find((item) => item.code === code);
+    return objCity.name;
+    
+};
+
+const getDate = (date) => {
+    return new Date(date).toLocaleString('ru', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+    });
+};
+
+const getChanges = (num) => {
+    if (num) {
+        return num === 1 ? 'С одной пересадкой' : 'С двумя пересадками';
+    } else {
+        return 'Без пересадок'
+    }
+};
+
+const getLinkAviasales = (data) => {
+    let link = 'https://www.aviasales.ru/search/'
+    link += data.origin
+    const date = new Date(data.depart_date);
+    const day = date.getDay();
+    const month = date.getMonth() + 1;
+    link += day < 10 ? '0' + day : day;
+    link += month < 10 ? '0' + month : month;
+    link += data.destination;
+    link += '1';
+    return link;
+};
+
+const createCard = (data) => {
+    const ticket = document.createElement('article');
+    ticket.classList.add('ticket');
+
+    let deep = '';
+
+    if (data) {
+        deep = `
+        <h3 class="agent">${data.gate}</h3>
+        <div class="ticket__wrapper">
+            <div class="left-side">
+                <a href="${getLinkAviasales(data)}" target="blank" class="button button__buy">Купить
+                    за ${data.value}₽</a>
+            </div>
+            <div class="right-side">
+                <div class="block-left">
+                    <div class="city__from">Вылет из города
+                        <span class="city__name">${getNameCity(data.origin)}</span>
+                    </div>
+                    <div class="date">${getDate(data.depart_date)}</div>
+                </div>
+
+                <div class="block-right">
+                    <div class="changes">${getChanges(data.number_of_changes)}</div>
+                    <div class="city__to">Город назначения:
+                        <span class="city__name">${getNameCity(data.destination)}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `;
+    } else {
+        deep = '<h3>Извините, на эту дату билетов нет</h3>'
+    }
+
+
+    ticket.insertAdjacentHTML('afterbegin', deep)
+
+    return ticket;
+};
+
+const renderCheapDay = (cheapTicket) => {
+
+    cheapestTicket.style.display = 'block';
+    cheapestTicket.innerHTML = '<h2>Самый дешевый билет от Aviasales</h2>';
+
+    const ticket = createCard(cheapTicket[0]);
+    cheapestTicket.append(ticket);
+    
+    
+};
+
+const renderCheapYear = (cheapTickets) => {
+
+    otherCheapTickets.style.display = 'block';
+    otherCheapTickets.innerHTML = '<h2>Самые дешевые билеты на другие даты</h2>';
+
+    cheapTickets.sort((a, b) => {
+        if (a.value > b.value) {
+          return 1;
+        }
+        if (a.value < b.value) {
+          return -1;
+        }
+        // a должно быть равным b
+        return 0;
+      });
+
+      for (let i = 0; i < cheapTickets.length && i < MAX_COUNT; i++) {
+          const ticket = createCard(cheapTickets[i]);
+          otherCheapTickets.append(ticket);
+      }
+ 
+};
+
+const renderCheap = (data, date) => {
+
+    const cheapTicketYear = JSON.parse(data).best_prices;
+    const cheapTicketDay = cheapTicketYear.filter((item) => {
+        return item.depart_date === date;
+    });  
+
+    renderCheapDay(cheapTicketDay);
+    // renderCheapYear(cheapTicketYear);
+
+
+}
+
+//skyScanner
+
+const getSkyCityIdByName = async (query) => {
+	let id;
+	let city;
+	let result = []
+
+	await fetch(`${myProxy}https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/?query=${query}`, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+		"x-rapidapi-key": "be4393eccamsh39d62f04ce5e4f8p1492acjsnfbbd8746a9b8"
+	}
+	})
+	.then((response) => {	
+		return response.json();
+	})
+	.then(function(data){
+        id = data.Places[0].CityId;
+        city = data.Places[0].PlaceName;
+		result.push(id);
+		result.push(city);
+	})
+	.catch(err => {
+		console.log(err);
+	});
+
+	return result;
+}
+
+const getSkyMinPrice = async (fromId, toId) => {
+	
+	let temporary;
+
+	await fetch(`${myProxy}https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/RUB/en-US/${fromId}/${toId}/${skyFetchDate}?`, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+		"x-rapidapi-key": "be4393eccamsh39d62f04ce5e4f8p1492acjsnfbbd8746a9b8"
+	}
+	})
+	.then(response => {
+		return response.json();
+	})
+	.then(function(data){
+		temporary = sortPrices(data);
+		minPrice = temporary[0].MinPrice;
+        dateSkyFrom = temporary[0].OutboundLeg.DepartureDate;
+        console.log(dateSkyFrom);
+        
+        
+	})
+	.catch(err => {
+		console.log(err);
+	});
+};
+
+const sortPrices = (data) => {
+	let prices = data.Quotes;
+	prices.sort(function (a, b) {
+        if (a.MinPrice > b.MinPrice) {
+          return 1;
+        }
+        if (a.MinPrice < b.MinPrice) {
+          return -1;
+        }
+        // a должно быть равным b
+        return 0;
+	  });
+	return prices;
+
+}
+
+const renderSkyCheapDay = () => {
+
+    cheapestTicketSky.style.display = 'block';
+    cheapestTicketSky.innerHTML = '<h2>Самый дешевый билет от SkyScanner</h2>';
+
+    const ticket = createSkyCard();
+    cheapestTicketSky.append(ticket);
+    
+    
+};
+
+const getSkyDate = (data) => {
+	let date = data.Quotes.DeparturDate;	
+}
+
+const createSkyCard = () => {
+	const ticket = document.createElement('article');
+    ticket.classList.add('ticket');
+
+    let deep = '';
+
+    if (1) {
+        deep = `
+        <h3 class="agent">Не удалось узнать провайдера</h3>
+        <div class="ticket__wrapper">
+            <div class="left-side">
+                <a href="${getSkyLink(startCityId, endCityId, dateSkyFrom)}" target="blank" class="button button__buy">Купить
+                    за ${minPrice}₽</a>
+            </div>
+            <div class="right-side">
+                <div class="block-left">
+                    <div class="city__from">Вылет из города
+                        <span class="city__name">${startCityName[0]}</span>
+                    </div>
+                    <div class="date">${getDate(dateSkyFrom)}</div>
+                </div>
+
+                <div class="block-right">
+                    <div class="changes">Неизвестно сколько пересадок</div>
+                    <div class="city__to">Город назначения:
+                        <span class="city__name">${endCityName[0]}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `;
+    } else {
+        deep = '<h3>Извините, на эту дату билетов нет</h3>'
+    }
+
+
+    ticket.insertAdjacentHTML('afterbegin', deep)
+
+    return ticket;
+}
+
+const selectSkyCity = (event, input, list, writeTo) => {
+
+    const target = event.target;
+    let skyCityFrom;
+
+    if (target.tagName.toLowerCase() === 'li') {
+        input.value = target.textContent;
+
+        skyCityFrom = city.find((item) =>{
+            return input.value === item.name;
+        })
+        list.textContent = '';
+
+        writeTo[0] = skyCityFrom.name_translations.en;    
+    }
+};
+
+const getSkyLink = (fromId, toId, dateFrom) => {
+	//   mosc/jfk/200901/
+    let link = 'https://www.skyscanner.ru/transport/flights/'
+	link += startCityId.slice(0, -4) + '/';
+	link += endCityId.slice(0, -4) + '/';
+	const date = new Date(dateSkyFrom);
+	const year = date.getFullYear();
+	const day = date.getDay();
+	const month = date.getMonth() + 1;
+	link += String(year).slice(0, -2);
+	link += month < 10 ? '0' + month : month;
+	link += day < 10 ? '0' + day : day;
+	link += '/';
+
+	return link;
+	
+}
+//Обработчики
+
+//aviasales
+inputCitiesFrom.addEventListener('input', () => {
+    showCity(inputCitiesFrom, dropdownCitiesFrom);
+});
+
+inputCitiesTo.addEventListener('input', () => {
+    showCity(inputCitiesTo, dropdownCitiesTo);
+});
+
+dropdownCitiesFrom.addEventListener('click', (event) => {
+    selectCity(event, inputCitiesFrom, dropdownCitiesFrom);
+});
+
+dropdownCitiesTo.addEventListener('click', (event) => {
+    selectCity(event, inputCitiesTo, dropdownCitiesTo);
+});
+
+formSearch.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    
+    
+
+    const cityFrom = city.find((item) => inputCitiesFrom.value === item.name);
+    const cityTo = city.find((item) => inputCitiesTo.value === item.name);
+
+    const formData = {
+        from: cityFrom,
+        to: cityTo,
+        when: inputDateDepart.value,
+    };
+
+    skyFetchDate = formData.when;
+
+
+    
+    if (formData.to && formData.from){
+        const requestData = `?depart_date=${formData.when}&origin=${formData.from.code}&destination=${formData.to.code}&one_way=true&token=${API_KEY}`;
+        getData(calendar + requestData, (response) => {
+            renderCheap(response, formData.when);
+            common();
+        }, error => {
+            alert('В этом направлении нет рейсов');
+            console.log('Ошибка', error);
+            
+        });
+    } else {
+        alert('Введите название города верно');
+    };
+});
+
+
+//skyscanner
+dropdownCitiesFrom.addEventListener('click', (event) => {
+    selectSkyCity(event, inputCitiesFrom, dropdownCitiesFrom, startCityName);
+});
+
+dropdownCitiesTo.addEventListener('click', (event) => {
+    selectSkyCity(event, inputCitiesTo, dropdownCitiesTo, endCityName);
+});
+
+// вызовы функций
+
+getData(myProxy + citiesApiFull, (data) => {
+        city = JSON.parse(data).filter(item =>  item.name);
+    
+        city.sort(function (a, b) {
+            if (a.name > b.name) {
+              return 1;
+            }
+            if (a.name < b.name) {
+              return -1;
+            }
+            // a должно быть равным b
+            return 0;
+          });
+    });
+
+const common = () => {
+
+    getSkyCityIdByName(startCityName)
+        .then(result => {
+            startCityId = result[0];
+        })
+        .then(() => getSkyCityIdByName(endCityName))
+        .then(result => {
+            endCityId = result[0];
+        })
+        .then(() => getSkyMinPrice(startCityId, endCityId))
+        .then(() => renderSkyCheapDay())
+    };
+    
